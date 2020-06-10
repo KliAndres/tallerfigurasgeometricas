8a25f6aade064b7c4d22f771953782f9a0a0f35f
Carlos Pacheco <cali.pacheco0826@gmail.com>
2020-06-09 22:44:19 -0500

    Agregado pagina principal, aplicacion funcional

---
+++ app/src/main/java/com/example/figurastaller/Principal.java
@@ -0,0 +1,22 @@
+package com.example.figurastaller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Principal extends AppCompatActivity {
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+    }
+
+    public void Entrar(View v){
+        intent =new Intent(Principal.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
